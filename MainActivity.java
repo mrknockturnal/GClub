588341491d1f0839f67dfe36b826dc5143c781f7
Moses Tjege <3566736@myuwc.ac.za>
2019-02-13 20:05:44 +0200

    Added comments to MainActivity

--- MainActivity.java
+++ MainActivity.java
@@ -5,6 +5,8 @@ public class MainActivity{
   private final int perRideBonus;
   private final int numSteps;
 
+
+  // Constructor to capture the values of the first line
   public MainActivity(int rows, int cols, int numVehicles, int perRideBonus, int numSteps){
     this.rows = rows;
     this.cols = cols;
@@ -15,7 +17,7 @@ public class MainActivity{
   }
 
   public static void main(String[] args){
-    
+    // This is where we need to read the text file
   }
 
 }
