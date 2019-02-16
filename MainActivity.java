a438cc29b73b1f0e7655944c4e794e6e081550c7
Moses Tjege <3566736@myuwc.ac.za>
2019-02-16 11:16:40 +0200

    Added reading file code
    I've written code to read the input file, convert the first line to an array of first type string then integer followed by using the 4 value in the array to determine how many rides are in the file and how many times we we will need to create Ride objects that must be stored in some e of list that can allow us to pop a ride once it's completed. Please feel free to look and modify if there are any changes

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+
 public class MainActivity{
-  private final int rows;
-  private final int cols;
-  private final int numVehicles;
-  private final int perRideBonus;
-  private final int numSteps;
+  public int rows;
+  public int cols;
+  public int numVehicles;
+  public int perRideBonus;
+  public int numSteps;
 
 
   // Constructor to capture the values of the first line
@@ -18,6 +22,42 @@ public class MainActivity{
 
   public static void main(String[] args){
     // This is where we need to read the text file
+    int numRides;
+    File file = new File("/home/mansa/Documents/Google_Hash_Code/Self_Driving_Rides/a_example.in");
+    try{
+
+      Scanner sc = new Scanner(file);
+      // The following code reads the first line of the input file
+      // and converts it to an integer array at the end.
+
+      String inputFormat = sc.nextLine();
+      String[] fLine = inputFormat.split(" ");
+      int[] integers = new int[fLine.length];
+      for (int i = 0; i < integers.length; i++){
+        integers[i] = Integer.parseInt(fLine[i]);
+      }
+      numRides = integers[3];
+      // The following code reads the remaining lines in the file
+      // and then converts each line to an integer array that represents the
+      // parameters of the ride i.e. row of start intersection, column of start intersection
+      // and so on.
+      for (int k = 0; k < numRides; k++){
+        String ride =sc.nextLine();
+        String[] sRide = ride.split(" ");
+        int[] rideParameters = new int[sRide.length];
+        for (int i = 0; i < rideParameters.length; i++){
+          rideParameters[i] = Integer.parseInt(sRide[i]);
+        }
+      }
+      System.out.println(numRides);
+      System.out.println(inputFormat);
+
+    }
+    catch (FileNotFoundException ex)
+    {
+      System.out.println("File does not exist");
+    }
+
   }
 
 }
